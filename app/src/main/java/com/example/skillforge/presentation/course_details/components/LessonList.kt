050aa7179adb09bc88ab9915b93a5b85759f6fa1
Vivek <shahvivek138@gmail.com>
2026-06-25 17:02:56 +0530

    Add lesson list composable

---
+++ app/src/main/java/com/example/skillforge/presentation/course_details/components/LessonList.kt
@@ -0,0 +1,29 @@
+package com.example.skillforge.presentation.course_details.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.skillforge.domain.model.LessonModel
+import com.example.skillforge.domain.model.dummyLessons
+
+
+@Composable
+fun LessonList(lessons: List<LessonModel>) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(lessons) {
+            LessonCard(it)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LessonListPreview() {
+
+    LessonList(dummyLessons)
+}
