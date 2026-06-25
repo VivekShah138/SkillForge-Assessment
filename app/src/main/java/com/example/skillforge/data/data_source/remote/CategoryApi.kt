4b6c52c787553719586c7d9c4311ab02013ec31d
Vivek <shahvivek138@gmail.com>
2026-06-25 22:35:30 +0530

    Add category api

--- app/src/main/java/com/example/skillforge/data/data_source/remote/CategoryApi.kt
+++ app/src/main/java/com/example/skillforge/data/data_source/remote/CategoryApi.kt
@@ -3,8 +3,8 @@ package com.example.skillforge.data.data_source.remote
 import com.example.skillforge.domain.model.response.CategoryResponse
 import retrofit2.http.GET
 
-//interface CategoryApi {
-//
-//    @GET
-//    suspend fun getAllCategories(): List<CategoryResponse>
-//}
+interface CategoryApi {
+
+    @GET
+    suspend fun getAllCategories(): List<CategoryResponse>
+}
