41c38746edb115751fee01edcf20e1e01c76d886
Vivek <shahvivek138@gmail.com>
2026-06-25 22:36:47 +0530

    provide retrofit category api instance via DI

--- app/src/main/java/com/example/skillforge/di/DataModule.kt
+++ app/src/main/java/com/example/skillforge/di/DataModule.kt
@@ -1,5 +1,8 @@
 package com.example.skillforge.di
 
+import com.example.skillforge.BuildConfig
+import com.example.skillforge.data.data_source.remote.CategoryApi
+import com.example.skillforge.utils.mapper.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,11 +17,11 @@ object DataModule {
 
     @Provides
     @Singleton
-    fun provideSignalApi(): SignalApi{
+    fun provideSignalApi(): CategoryApi{
         return Retrofit.Builder()
-            .baseUrl(BuildConfig)
+            .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(SignalApi::class.java)
+            .create(CategoryApi::class.java)
     }
 }
