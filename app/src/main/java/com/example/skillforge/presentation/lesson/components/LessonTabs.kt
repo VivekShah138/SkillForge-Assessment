b1caef32fa3f496f0f918509b42a676ac50cd33f
Vivek <shahvivek138@gmail.com>
2026-06-26 11:26:25 +0530

    Update Lesson Tabs

--- app/src/main/java/com/example/skillforge/presentation/lesson/components/LessonTabs.kt
+++ app/src/main/java/com/example/skillforge/presentation/lesson/components/LessonTabs.kt
@@ -1,9 +1,17 @@
 package com.example.skillforge.presentation.lesson.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -12,23 +20,44 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun LessonTabs() {
+    var selectedTab by remember { mutableIntStateOf(0) }
+
+    val tabs = listOf("Lessons", "Notes", "Resources")
+
     Column {
         Row(
             horizontalArrangement = Arrangement.spacedBy(32.dp)
         ) {
-            Text("Lessons", fontWeight = FontWeight.Bold)
-            Text("Notes", color = Color.Gray)
-            Text("Resources", color = Color.Gray)
+            tabs.forEachIndexed { index, title ->
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .clickable { selectedTab = index }
+                ) {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.titleSmall.copy(
+                            fontWeight = if (selectedTab == index) FontWeight.Bold else FontWeight.Normal,
+                            color = if (selectedTab == index) Color.Black else Color.Gray
+                        )
+                    )
+
+                    Spacer(Modifier.height(8.dp))
+
+                    Box(
+                        modifier = Modifier
+                            .width(60.dp)
+                            .height(3.dp)
+                            .background(
+                                if (selectedTab == index)
+                                    Color(0xFF00B8A9)
+                                else
+                                    Color.Transparent
+                            )
+                    )
+                }
+            }
         }
-
-        Spacer(Modifier.height(8.dp))
-
-        Box(
-            modifier = Modifier
-                .width(60.dp)
-                .height(3.dp)
-                .background(Color(0xFF00B8A9))
-        )
     }
 }
 
